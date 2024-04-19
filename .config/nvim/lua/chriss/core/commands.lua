d53f12d551fcaee7b09cd8c3fdb12b2f4409bd71
Chris Stromquist <chriss@clinicomp.com>
2024-04-18 22:44:37 -0700

    update gitignore. add commands.

--- .config/nvim/lua/chriss/core/commands.lua
+++ .config/nvim/lua/chriss/core/commands.lua
@@ -1,2 +1,5 @@
 -- Define the autocommands
 vim.cmd("autocmd BufNewFile,BufRead *.ycql,*.lib,*.func :set filetype=sql")
+vim.cmd("set foldmethod=indent")
+vim.cmd("set foldlevel=1")
+vim.cmd("set foldclose=all")
--- .config/nvim/lua/chriss/core/syntax.lua
+++
@@ -1,3 +0,0 @@
--- Define the autocommands
-vim.cmd("autocmd BufNewFile,BufRead *.ycql,*.lib,*.func :set filetype=sql")
-print("Lua syntax configuration loaded")
