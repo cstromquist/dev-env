831f75f93e13f690dfc5b3baa0894de0af22ed95
Chris Stromquist <chriss@clinicomp.com>
2024-04-23 21:14:20 -0700

    add surround plugin. update configs.

--- .config/nvim/lua/chriss/core/commands.lua
+++ .config/nvim/lua/chriss/core/commands.lua
@@ -1,5 +1,5 @@
 -- Define the autocommands
 vim.cmd("autocmd BufNewFile,BufRead *.ycql,*.lib,*.func :set filetype=sql")
 vim.cmd("set foldmethod=indent")
-vim.cmd("set foldlevel=10")
+vim.cmd("set foldlevel=20")
 -- vim.cmd("set foldclose=all")
