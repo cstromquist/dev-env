4bf6ef31ea53a5483d1cbbec30bdaebd1d83b658
Chris Stromquist <chriss@clinicomp.com>
2024-05-02 16:19:46 -0700

    add mouse control for tmux. add display filename to telescope.

--- .config/nvim/lua/chriss/plugins/telescope.lua
+++ .config/nvim/lua/chriss/plugins/telescope.lua
@@ -16,15 +16,52 @@ return {
 		local telescope = require("telescope")
 		local actions = require("telescope.actions")
 
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "TelescopeResults",
+			callback = function(ctx)
+				vim.api.nvim_buf_call(ctx.buf, function()
+					vim.fn.matchadd("TelescopeParent", "\t\t.*$")
+					vim.api.nvim_set_hl(0, "TelescopeParent", { link = "Comment" })
+				end)
+			end,
+		})
+
+		local function filenameFirst(_, path)
+			local tail = vim.fs.basename(path)
+			local parent = vim.fs.dirname(path)
+			if parent == "." then
+				return tail
+			end
+			return string.format("%s\t\t%s", tail, parent)
+		end
+
 		telescope.setup({
 			defaults = {
-				path_display = { "smart" },
+				-- path_display = { "smart" },
+				-- path_display = function(opts, path)
+				-- 	local tail = require("telescope.utils").path_tail(path)
+				-- 	return string.format("%s (%s)", tail, path)
+				-- end,
+				-- Format path as "file.txt (path\to\file\)"
+				-- path_display = function(opts, path)
+				-- 	local tail = require("telescope.utils").path_tail(path)
+				-- 	return string.format("%s (%s)", tail, path), { { { 1, #tail }, "Constant" } }
+				-- end,
+				path_display = filenameFirst,
 				mappings = {
 					i = {
 						["<C-k>"] = actions.move_selection_previous, -- move to prev result
 						["<C-j>"] = actions.move_selection_next, -- move to next result
 						["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+						["<c-f>"] = actions.preview_scrolling_down,
+						["<c-b>"] = actions.preview_scrolling_up,
+						["<c-p>"] = require("telescope.actions.layout").toggle_preview,
 					},
+				},
+			},
+			pickers = {
+				find_files = {
+					path_display = filenameFirst,
 				},
 			},
 		})
